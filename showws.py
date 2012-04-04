65944a645070010a1ce1b5d470c0c85486394af0
Evan Lawrence-Hurt <evanlh@gmail.com>
2012-04-04 15:38:54 -0400

    Fixing up bootstrap

--- showws.py
+++ showws.py
@@ -1,8 +1,15 @@
-from flask import Flask, request, session, render_template
+from flask import Flask, request, session, jsonify, render_template
 app = Flask(__name__)
 
+@app.route("/v1/shows", methods = ['GET', 'POST'])
+def shows():
+    if request.method == "POST":
+        pass
+    else:
+        pass
+
 @app.route("/")
-def showws():
+def index():
     return render_template('layout.html')
 
 if __name__ == "__main__":
